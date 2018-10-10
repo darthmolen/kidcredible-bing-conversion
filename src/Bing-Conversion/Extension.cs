47fe166d06cb53276eed5fd3e67a94478ed28ba9
Steven Molen <darthmolen@gmail.com>
2018-10-10 12:39:49 -0500

    Initial Commit

---
+++ src/Bing-Conversion/Extension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bing.Conversion
+{
+    public static class Extension
+    {
+        public static string ToCommaDelimited(this List<string> list)
+        {
+            return String.Join(',', list);
+        }
+    }
+}
---
+++ src/Bing-Conversion/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Bing.Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            Console.WriteLine("Hello World!");
+            var foundIds = new List<string>();
+
+
+            // Select a record
+            var sql = $"SELECT * FROM Location WHERE Id not in ({foundIds.ToCommaDelimited()}) Limit 1;";
+
+            // Query Bing
+            // https://msdn.microsoft.com/en-us/library/hh478192.aspx#How%20to%20query%20the%20NAVTEQNA%20data%20source
+
+        }
+    }
+}
